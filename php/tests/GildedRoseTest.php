93b30a76379499849db72d7619818847248172e3
David Talavera <dtala86@gmail.com>
2022-07-19 21:36:25 +0200

    Standard item tests

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -44,4 +44,31 @@ class GildedRoseTest extends TestCase
         $this->assertSame(0, $items[0]->sell_in);
         $this->assertSame(80, $items[0]->quality);
     }
+
+    public function test_standard_item_before_sell_in_date_lowers_both_values_by_one(): void
+    {
+        $items = [new Item('Elixir of the Mongoose', 10, 10)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(9, $items[0]->sell_in);
+        $this->assertSame(9, $items[0]->quality);
+    }
+
+    public function test_standard_item_after_sell_in_date_quality_lowers_twice_as_fast(): void
+    {
+        $items = [new Item('Elixir of the Mongoose', -1, 10)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(-2, $items[0]->sell_in);
+        $this->assertSame(8, $items[0]->quality);
+    }
+
+    public function test_standard_item_on_sell_in_date_quality_lowers_twice_as_fast(): void
+    {
+        $items = [new Item('Elixir of the Mongoose', 0, 10)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(-1, $items[0]->sell_in);
+        $this->assertSame(8, $items[0]->quality);
+    }
 }
